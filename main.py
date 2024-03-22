8457386f198f2d9f4a5ec12d1fb8e67f3ed07e68
rohit958 <rohitkushwaha630@gmail.com>
2024-03-22 11:52:58 +0530

    load JSON

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 import pandas as pd
+import json
+from pandasql import sqldf
 
-df=pd.read_json(r"C:\git\rohit958\WeatherData\data\weather_data.json")
-df.head(3)
+with open(r"C:\git\rohit958\WeatherData\data\weather_data.json",encoding='utf-8', errors='ignore') as f:
+    json_obj=json.loads(f.read(),strict=False)
+
+
+#creating pandas dataframe
+df=pd.read_json(json_obj)
+print(df.head(3))
+
+destination=r"C:\git\rohit958\WeatherData\final_csv\final.csv"
+try:
+    df.to_csv(destination,sep='|',header=True,index=True)
+    print('file created')
+except FileExistsError:
+    print('failed')
+finally:
+
+    print("exting")
+    exit()
